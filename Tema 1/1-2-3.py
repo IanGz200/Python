b00ed02809cbcb9520e9ee41a13f32866858ed1c
IanGz200 <iangraziano18@gmail.com>
2025-09-22 13:25:40 +0200

    and or

--- Tema 1/1-2-3.py
+++ Tema 1/1-2-3.py
@@ -2,6 +2,6 @@
 Clase para evaluar una Expresión regular
 """
 class Regexp:
-    expre= 3+5<5*2|3>8&7>6-2
+    expre= 3+5<5*2 or 3>8 and 7>6-2
     #Muestra el resultado de la expresión regular
     print(expre)
