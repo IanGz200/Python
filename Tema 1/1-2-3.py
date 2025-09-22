1740dcb021262ba24bb6a39c65245dd3edc97f60
IanGz200 <iangraziano18@gmail.com>
2025-09-22 13:28:13 +0200

    correccion palabra

--- Tema 1/1-2-3.py
+++ Tema 1/1-2-3.py
@@ -1,7 +1,7 @@
 """
 Clase para evaluar una Expresión lógica
 """
-class Regexp:
+class Logexp:
     expre= 3+5<5*2 or 3>8 and 7>6-2
     #Muestra el resultado de la expresión lógica
     print(expre)
