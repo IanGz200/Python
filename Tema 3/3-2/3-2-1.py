5fa351cb6d90388bfb71c1272d35db6f2e385a98
IanGz200 <iangraziano18@gmail.com>
2025-11-24 16:08:29 +0100

    3-2 Comienzo

---
+++ Tema 3/3-2/3-2-1.py
@@ -0,0 +1,23 @@
+class Matrix:
+    matrix:list[list[int]]=[[0,0,0],[0,0,0],[0,0,0]]
+
+    def __init__(self) -> None:
+        for row in range(3):
+            for col in range(3):
+                self.matrix[row][col] = int(input(f"Introduce el numero {row + 1},{col + 1}: "))
+
+
+    def sum(self,m2)->list[list[int]]:
+        for row in range(len(self.matrix)):
+            for col in range(len(self.matrix[row])):
+                self.matrix[row][col] += m2[row][col]
+
+        return self.matrix
+
+def main():
+    m = Matrix()
+    m2 = Matrix()
+    print(m.sum(m2.matrix))
+
+if __name__ == "__main__":
+    main()
