fc485c45fdd43f4a997a9a38d38fcff7c5e00a12
IanGz200 <iangraziano18@gmail.com>
2025-12-01 13:22:52 +0100

    3-2-1 Corregida

--- Tema 3/3-2/3-2-1.py
+++ Tema 3/3-2/3-2-1.py
@@ -2,30 +2,45 @@ class Matrix:
     """
     Clase que implementa una matriz y metodos para hacer calculo entre estas
     """
-    matrix:list[list[int]]=[[0,0,0],[0,0,0],[0,0,0]]
+    matrix:list[list[int]]
 
-    def __init__(self) -> None:
+    def fill(self) -> None:
+        self.matrix = []
         for row in range(3):
+            self.matrix.append([])
             for col in range(3):
-                self.matrix[row][col] = int(input(f"Introduce el numero {row + 1},{col + 1}: "))
+                self.matrix[row].append(int(input(f"Introduce el numero {row + 1},{col + 1}: ")))
 
 
-    def sum(self,m2)->list[list[int]]:
+    def sum(self,m2:'Matrix')->'Matrix':
         """
         Suma dos matrices casilla por casilla
         :param m2: Una matriz
         :return: La suma de ambas matrices
         """
+        result = Matrix()
+        result.matrix = []
+        for row in range(len(self.matrix)):
+            result.matrix.append([])
+            for col in range(len(self.matrix[row])):
+                result.matrix[row].append(self.matrix[row][col]+m2.matrix[row][col])
+
+        return result
+
+    def show(self):
         for row in range(len(self.matrix)):
             for col in range(len(self.matrix[row])):
-                self.matrix[row][col] += m2[row][col]
-
-        return self.matrix
+                print(self.matrix[row][col], end=" ")
+            print()
 
 def main():
+    print('Introduce os valores da primeira matriz:')
     m = Matrix()
+    m.fill()
+    print('Introduce os valores da segunda matriz:')
     m2 = Matrix()
-    print(m.sum(m2.matrix))
+    m2.fill()
+    m.sum(m2).show()
 
 if __name__ == "__main__":
     main()
